2594e2da97f8d25d7a1669f4da9a24e0046ff057
Front-End <alikeremovsahin@gmail.com>
2025-04-23 21:54:08 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let name = String(prompt());
+console.log(name.length);
+console.log(name.includes("e"));
+console.log(name.startsWith("code"));
+console.log(name.endsWith("code"));
+console.log(name.replaceAll("t", "####"));
